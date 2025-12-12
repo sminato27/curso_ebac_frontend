439069e2548d61c5e1705b3894136f94beeacb98
Minato <59450675+sminato27@users.noreply.github.com>
2025-12-12 14:18:43 -0300

    Realização do exercício

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+function multiplicacao (n1: number, n2: number) {
+    return console.log(n1 * n2);
+}
+
+multiplicacao(2, 2);
+
+function saudacao (nome: string) {
+    return console.log("Olá " + nome);
+}
+
+saudacao("Juliete");
